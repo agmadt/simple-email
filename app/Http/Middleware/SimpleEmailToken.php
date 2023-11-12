f7ed45d09bad3e0a07f8f1d6b65c28e8f047749c
Agam Adhityo <adhityoagam@gmail.com>
2023-11-12 21:14:34 +0700

    :sparkles: missing token message clarification

--- app/Http/Middleware/SimpleEmailToken.php
+++ app/Http/Middleware/SimpleEmailToken.php
@@ -16,9 +16,15 @@ class SimpleEmailToken
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!$request->has('api_token') || $request->api_token != config('auth.api.token')) {
+        if (!$request->has('api_token')) {
             return response()->json([
-                'message' => 'Unauthorized. Missing API Token'
+                'message' => 'Unauthorized. Missing API Token.'
+            ], 401);
+        }
+
+        if ($request->api_token != config('auth.api.token')) {
+            return response()->json([
+                'message' => 'Unauthorized. API Token Mismatch.'
             ], 401);
         }
 
